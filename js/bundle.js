b48f666346e869a06e7eba177c312051f6ca7a2f
Peter Strumolo <Peter Strumolo>
2015-12-19 14:16:59 -0500

    Adds blog post and moment post

--- js/bundle.js
+++ js/bundle.js
@@ -42,50 +42,9 @@
 /************************************************************************/
 /******/ ([
 /* 0 */
-/***/ function(module, exports, __webpack_require__) {
-
-	__webpack_require__(1);
-
-
-/***/ },
-/* 1 */
 /***/ function(module, exports) {
 
-	// When I click on 'Portfolio':
-	// - Hide 'about'
-	// - Show 'portfolio'
-
-	(function () {
-
-	  var home = document.getElementById('home');
-	  var portfolio = document.getElementById('portfolio');
-	  var messages = document.getElementById('messages');
-	  var blog = document.getElementById('blog');
-
-	  var pages = [home, portfolio, messages, blog];
-
-	  var portfolioButton = document.getElementById('portfolio-btn');
-
-	  function hidePages(keepPage) {
-
-	    for(var i = 0; i < pages.length; i++) {
-	      if (pages[i] !== keepPage) {
-	        pages[i].style.display = 'none';
-	      }
-	    }
-	  
-	  }
-
-	  portfolioButton.addEventListener("click", function(e) {
-	    e.preventDefault();
-
-	    hidePages(portfolio);
-
-	    console.log("Portfolio Appears!");
-	  });
-
-	})();
-
+	
 
 /***/ }
 /******/ ]);
