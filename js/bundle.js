1407b7663cde1e3abcc6d058bcb6b5ddb53407ef
pstrum <pstrum@gmail.com>
2018-02-27 11:32:46 -0800

    Add new journal map

--- js/bundle.js
+++ js/bundle.js
@@ -125,21 +125,75 @@
 	  var lng = $("#map").data("lng");
 
 	  window.initMap = function() {
-	    console.log("yolo");
-	    var map = new google.maps.Map(document.getElementById('map'), {
+	    var mapOptions = {
 	      zoom: 15,
 	      center: {lat: parseFloat(lat), lng: parseFloat(lng)},
 	      disableDefaultUI: true,
 	      draggable: false,
 	      scrollwheel: false
-	    });
-	    var geocoder = new google.maps.Geocoder;
-	    geocodeLatLng(geocoder, map);
-	    google.maps.event.addDomListener(window, "resize", function() {
-	      var center = map.getCenter();
-	      google.maps.event.trigger(map, "resize");
-	      map.setCenter(center); 
-	    });
+	    };
+
+	    if (lat === 'markerMap') {
+	      var bounds = new google.maps.LatLngBounds();
+	      var $entryList = $("#map-entry-list li");
+	      var bounds = new google.maps.LatLngBounds();
+	      var locations = [];
+	      $entryList.each(function getLocations() {
+	        var entryLat = $(this).data("entry-lat");
+	        var entryLng = $(this).data("entry-lng");
+	        var xCallbackUrl = $(this).data("entry");
+	        var date = $(this).data("date");
+	        locations.push({
+	          location: {
+	            lat: entryLat,
+	            lng: entryLng
+	          },
+	          entryUrl: xCallbackUrl,
+	          entryDate: date
+	        });
+	      });
+
+	      mapOptions = {
+	        zoom: 2,
+	        center: {lat: 39.431728, lng: -109.797011},
+	        disableDefaultUI: true,
+	        gestureHandling: 'cooperative'
+	      };
+
+	      var map = new google.maps.Map(document.getElementById('map'), mapOptions);
+	      var markers = locations.map(function(location) {
+	        var entry = '<a href="' + location.entryUrl + '">' + location.entryDate + '</a>';
+	        var infoWindow = new google.maps.InfoWindow({
+	          content: entry
+	        });
+	        var marker = new google.maps.Marker({
+	          position: location.location,
+	          icon: '/icons/location-1.svg'
+	        });
+	        marker.addListener('click', function() {
+	          infoWindow.open(map, marker);
+	        });
+	        return marker;
+	      });
+	      var markerCluster = new MarkerClusterer(map, markers, {
+	        styles: [{
+	          textColor: 'white',
+	          width: '53',
+	          height: '52',
+	          url: '/images/m1.png'
+	        }]
+	      });
+	      var styles = markerCluster.getStyles();
+	    } else {
+	      var map = new google.maps.Map(document.getElementById('map'), mapOptions);
+	      var geocoder = new google.maps.Geocoder;
+	      geocodeLatLng(geocoder, map);
+	      google.maps.event.addDomListener(window, "resize", function() {
+	        var center = map.getCenter();
+	        google.maps.event.trigger(map, "resize");
+	        map.setCenter(center);
+	      });
+	    }
 	  };
 
 	  function loadGoogle() {
@@ -171,9 +225,7 @@
 	  }
 
 	  loadGoogle();
-
 	})();
-
 
 
 /***/ },
@@ -181,8 +233,9 @@
 /***/ function(module, exports) {
 
 	module.exports = {
-	  googleKey: 'AIzaSyBqmF71sk9AOaG3lfdKJlwyB81plCq6onk',
+	  googleKey: 'AIzaSyBqmF71sk9AOaG3lfdKJlwyB81plCq6onk'
 	};
+
 
 
 /***/ }
